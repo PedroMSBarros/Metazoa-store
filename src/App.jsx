6fd06874d29f15f198e3fda24bd4eb772128a194
Pedro Moreira da Silva Barros <pedromsilvabarros2001@gmail.com>
2026-03-15 14:59:35 -0300

    adiciona botao flutuante do whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Catalogo from './pages/Catalogo'
 import Sobre from './pages/Sobre'
 import Cuidados from './pages/Cuidados'
 import PeixeDetalhe from './pages/PeixeDetalhe'
+import WhatsAppFloat from './components/WhatsAppFloat'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/catalogo" element={<Catalogo />} />
-      <Route path="/sobre" element={<Sobre />} />
-      <Route path="/cuidados" element={<Cuidados />} />
-      <Route path="/peixe/:id" element={<PeixeDetalhe />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/cuidados" element={<Cuidados />} />
+        <Route path="/peixe/:id" element={<PeixeDetalhe />} />
+      </Routes>
+      <WhatsAppFloat />
+    </>
   )
 }
 
